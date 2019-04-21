bd1c9e975cf78140a4176c26084710a15bb1938e
Emir Eminovic <eeminovi@gmail.com>
2019-04-21 21:43:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+var holiday;
+var name;
+function happyHolidays(){
+  return "Happy holidays!";
+}
+function happyHolidaysTo(string){
+  var state= 'Happy holidays, you!'
+  return state;
+}
+
+function happyHolidayTo(holiday,name){
+  var state= 'Happy ${holiday}, ${name}!'
+  return state;
+}
+
