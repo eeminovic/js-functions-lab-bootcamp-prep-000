d5f138faa69dcd28e3bdc30b18987f980260ed77
Emir Eminovic <eeminovi@gmail.com>
2019-04-21 21:45:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function happyHolidayTo(holiday,name){
   return state;
 }
 
-function holidayCountDown(holiday, days){
+function holidayCountdown(holiday, days){
   return "It's ${days} days until${holiday}!"
 }
